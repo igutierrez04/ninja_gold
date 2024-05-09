dda472405c92a4c7ea937711286e261f5fda3c29
Ivan <igutierrez0794@gmail.com>
2024-05-08 22:21:44 -0400

    completed project. added app route without 4 conditionals, created logic to display reset button on win or lose

--- server.py
+++ server.py
@@ -11,36 +11,73 @@ def random_gold(num1, num2):
 
 @app.route('/')
 def home():
-    max_gold = 150
+    max_gold = 500
     if 'gold' not in session:
         session['gold'] = 0
     if 'activity' not in session:
         session['activity'] = []
-    return render_template('index.html')
+    if 'count' not in session:
+        session['count'] = 0
+    return render_template('index.html', max_gold = max_gold)
 
+
+# without 4 conditionals
 @app.route('/process_money', methods=['POST'])
 def process_money():
-    print("recieved")
-    if request.form['money'] == 'farm':
-        gold_found = random_gold(10,20)
-        session['gold'] += gold_found
-        color = 'success'
-    elif request.form['money'] == 'cave':
-        gold_found = random_gold(5,10)
-        session['gold'] += gold_found
-        color = 'success'
-    elif request.form['money'] == 'house':
-        gold_found = random_gold(2,5)
-        session['gold'] += gold_found
-        color = 'success'
-    else:
-        gold_found = random_gold(-50,50)
-        session['gold'] += gold_found
+    farming_dict = {
+        'farm': [10,20],
+        'cave': [5,10],
+        'house': [2,5],
+        'casino': [-50,50]
+    }
+    if request.form['money'] in farming_dict:
+        gold_found = random_gold(farming_dict[request.form['money']][0], farming_dict[request.form['money']][1])
         if gold_found < 0:
             color = 'danger'
-        else: color = 'success'
-    session['activity'].insert(0,[f'Earned {gold_found} gold from the {request.form["money"]} - {datetime.now().strftime("%Y/%m/%d %I:%M %p")}', color])
-    print(session['gold'])
+        else:
+            color = 'success'
+        session['gold'] += gold_found
+        session['activity'].insert(0,[f'Earned {gold_found} gold from the {request.form["money"]} - {datetime.now().strftime("%Y/%m/%d %I:%M %p")}', color])
+        session['count'] += 1
+    return redirect('/')
+
+# with 4 conditionals
+# @app.route('/process_money', methods=['POST'])
+# def process_money():
+#     print("recieved")
+#     count = 0
+#     if request.form['money'] == 'farm':
+#         gold_found = random_gold(10,20)
+#         session['gold'] += gold_found
+#         session['count'] += 1
+#         color = 'success'
+#     elif request.form['money'] == 'cave':
+#         gold_found = random_gold(5,10)
+#         session['gold'] += gold_found
+#         session['count'] += 1
+#         color = 'success'
+#     elif request.form['money'] == 'house':
+#         gold_found = random_gold(2,5)
+#         session['gold'] += gold_found
+#         session['count'] += 1
+#         color = 'success'
+#     else:
+#         gold_found = random_gold(-50,50)
+#         session['gold'] += gold_found
+#         if gold_found < 0:
+#             session['count'] += 1
+#             color = 'danger'
+#         else:
+#             session['count'] += 1
+#             color = 'success'
+
+#     session['activity'].insert(0,[f'Earned {gold_found} gold from the {request.form["money"]} - {datetime.now().strftime("%Y/%m/%d %I:%M %p")}', color])
+#     print(session['count'])
+#     return redirect('/')
+
+@app.route('/reset')
+def reset():
+    session.clear()
     return redirect('/')
 
 
